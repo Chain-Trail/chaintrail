c6f428f92bbc9c596f4a0b2089afee7b4eb32067
Damian <olebueziedamian@gmail.com>
2024-07-28 04:51:13 +0100

    ui

--- app/play/quest-1/Question1.jsx
+++ app/play/quest-1/Question1.jsx
@@ -3,9 +3,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { CiCircleCheck } from "react-icons/ci";
 import { RiMenu3Fill } from "react-icons/ri";
-import { GrFormNextLink } from "react-icons/gr";
 import { MdDelete } from "react-icons/md";
 import Points from "@/app/components/user/Points";
 import { questQuestions } from "./questions";
@@ -89,79 +87,74 @@ const QuestionComponent = () => {
   }, [showPopup, isCompleted]);
 
   return (
-    <section>
-        <QuesUI/>
+    <section className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <QuesUI />
       {/* header starts */}
-      <div className=" py-2 flex justify-between font-bold items-center text-sm">
+      <div className="py-4 flex justify-between items-center text-sm">
         <div className="flex items-center gap-2">
-          <img src="../chaincoins.svg" alt="Chain Coins" />
-          <span>
+          <img src="../chaincoins.svg" alt="Chain Coins" className="w-6 h-6" />
+          <span className="font-bold">
             <Points />
           </span>
         </div>
-        <div className="flex">
-          <img src="../redImg.png" alt="level" className="w-[40px]" />
-          <span className="relative text-xs top-[12px] right-[30px]">
+        <div className="flex items-center">
+          <img src="../redImg.png" alt="level" className="w-12 h-11" />
+          <span className="relative text-xs -ml-7 mt-1 font-bold">
             {currentQuestionIndex + 1}/{questions.length}
           </span>
         </div>
-        <div className="text-2xl flex gap-2 font-extrabold ">
-          <Link href="/">
+        <div className="text-2xl">
+          <Link href="/" className="block">
             <RiMenu3Fill title="home" />
           </Link>
         </div>
       </div>
       {/* header ends and image list starts */}
-      <section className="max-w-[320px] px-2">
-        <div className="grid grid-cols-2 gap-2 place-content-center w-full mx-auto px-4">
+      <section className="max-w-[320px]">
+        <div className="grid grid-cols-2 gap-4">
           {currentQuestion.images.map((image, index) => (
             <img
               key={index}
               loading="lazy"
               src={image}
-              className=" rounded-lg w-full max-h-[140px] hover:brightness-50"
+              className="rounded-lg w-full h-32 object-cover hover:brightness-75 transition-all"
               alt={`question ${currentQuestionIndex + 1} image ${index + 1}`}
             />
           ))}
         </div>
         {/* style for input area */}
-        <div className="max-w-[300px] mx-auto bg-neutral-950 text-white px-1 py-2 my-1 flex items-center text-lg">
-          <div className="flex mx-auto">
+        <div className="text-white py-4 flex justify-center">
+          <div className="flex">
             {Array.from({ length: currentQuestion.correctAnswer.length }).map(
               (_, index) => (
                 <span
                   key={index}
-                  className="w-7 h-8 bg-[#404040] rounded mx-1 flex items-center justify-center">
+                  className="w-8 h-10 bg-gray-700 rounded mx-1 flex items-center justify-center text-lg">
                   {selectedAnswers[index] || ""}
                 </span>
               )
             )}
           </div>
         </div>
-        <div className="mx-auto text-xl max-w-[320px] gap-4 my-2">
-          <div className="flex flex-wrap items-center gap-2 justify-center">
+        <div className="my-4">
+          <div className="flex flex-wrap justify-center gap-2">
             {currentQuestion.possibleAnswers.map((answer, index) => (
-              <div
+              <button
                 key={index}
-                className="bg-black hover:bg-yellow-700 active:bg-yellow-900 text-white p-2 rounded-md cursor-pointer"
+                className="bg-black hover:bg-yellow-700 active:bg-yellow-900 text-white py-2 px-4 rounded-md text-lg"
                 onClick={() => handleAnswerClick(answer)}>
                 {answer}
-              </div>
+              </button>
             ))}
           </div>
         </div>
-        <div className="max-w-[320px] font-bold flex items-center text-lg justify-center py-2 gap-2 mx-auto">
+        <div className="flex items-center gap-4 my-4">
           <button
             onClick={openModal}
-            className="bg-white text-sm text-black py-2 px-2 rounded ">
+            className="flex-grow bg-white text-black py-2 px-4 rounded text-sm font-bold flex items-center justify-center">
             GET HINT WITH
-            <img
-              src="../chaincoins.svg"
-              width={25}
-              className="inline mx-2 object-cover w-[20px]"
-              alt="hint"
-            />
-            20
+            <img src="../chaincoins.svg" className="w-5 h-8 ml-2" alt="hint" />
+            <span className="ml-1">20</span>
           </button>
           <Modal
             isOpen={isModalOpen}
@@ -169,25 +162,12 @@ const QuestionComponent = () => {
             onClose={closeModal}
           />
           <button
-            className="bg-red-700 active:bg-red-500 px-4 flex flex-col py-1 rounded items-center justify-between cursor-pointer"
+            className="flex-shrink-0 bg-red-700 active:bg-red-500 px-4 py-2 rounded flex flex-col items-center justify-center"
             onClick={deleteLast}>
-            <MdDelete />
-            <span className="text-xs">Del </span>
+            <MdDelete className="text-xl" />
+            <span className="text-xs">Del</span>
           </button>
-          <span
-            className="bg-green-700 active:bg-green-500 flex flex-col px-2 py-1 rounded items-center justify-between cursor-pointer"
-            onClick={handleNext}>
-            {currentQuestionIndex === questions.length - 1 ? (
-              <>
-                <CiCircleCheck /> <span className="text-xs">Finish </span>
-              </>
-            ) : (
-              <>
-                <GrFormNextLink /> <span className="text-xs">Next </span>
-              </>
-            )}
-          </span>
-        </div>
+        </div>{" "}
         {showPopup && (
           <div>
             {isCompleted && showComplete ? (
@@ -206,23 +186,20 @@ const QuestionComponent = () => {
 
 export default QuestionComponent;
 
-
-const QuesUI =()=>{
-    return (
-      <div className="fixed inset-0 -z-30 flex items-center justify-center bg-black  font-bold text-lg">
-        <div className="relative w-screen h-screen ">
-          <Image
-            src="/btn/success.png"
-            alt="success background"
-            width={1000}
-            height={1000}
-            className="absolute inset-0 w-full h-full "
-          />
-          <div className="absolute inset-0 bg-black bg-opacity-80"></div>
-          <div className="absolute inset-0 flex items-center justify-center">
-
-          </div>
-        </div>
+const QuesUI = () => {
+  return (
+    <div className="fixed inset-0 -z-30 flex items-center justify-center bg-black  font-bold text-lg">
+      <div className="relative w-screen h-screen ">
+        <Image
+          src="/btn/success.png"
+          alt="success background"
+          width={1000}
+          height={1000}
+          className="absolute inset-0 w-full h-full "
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-80"></div>
+        <div className="absolute inset-0 flex items-center justify-center"></div>
       </div>
-    );
-}
+    </div>
+  );
+};
