a5c4121ed9331341a2a632a4073d4711f5eeb3b3
Damian <65533328+czDamian@users.noreply.github.com>
2024-07-28 09:43:27 -0700

    Update page.jsx

--- app/Admin/new-question/new-quest/page.jsx
+++ app/Admin/new-question/new-quest/page.jsx
@@ -1,1 +1,11 @@
 
+// Admin/page.js
+import CreateQuest from './CreateQuest';
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-4">
+      <CreateQuest />
+    </div>
+  );
+}
