17fddbaf7bfcbb7de5d18261cf373bacc6322136
Damian <65533328+czDamian@users.noreply.github.com>
2024-07-28 09:42:53 -0700

    Update page.jsx

--- app/Admin/new-question/page.jsx
+++ app/Admin/new-question/page.jsx
@@ -1,11 +1,9 @@
 // Admin/page.js
-import CreateQuest from './CreateQuest';
 import CreateQuestQuestion from './CreateQuestQuestions';
 
 export default function Home() {
   return (
     <div className="container mx-auto px-4">
-      <CreateQuest />
       <CreateQuestQuestion />
     </div>
   );
